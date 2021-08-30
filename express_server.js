5cd64480e26ccfe6c47499ef2560b8964c167000
Juan <juantvaldez85.@gmail.com>
2021-08-30 02:02:32 +0000

    URL Shortening (Part 1)

--- express_server.js
+++ express_server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8090; // default port 8090
+const bodyParser = require("body-parser");
+
+function generateRandomString() {
+  let randomString = (Math.random() + 1).toString(36).substring(7);
+  console.log("random", randomString);
+}
+generateRandomString();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -20,6 +29,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -43,6 +56,11 @@ app.get("/set", (req, res) => {
 
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
